89d209600ec0f22626c6a212049b818ca93ee579
valerijmedvid <vmedvid@gmail.com>
2023-02-19 10:38:36 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDo app
+
+![Deploy](https://github.com/valerijmedvid/todo/workflows/Deploy/badge.svg)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>ToDo App</h1>
+    </div>
+  )
+}
