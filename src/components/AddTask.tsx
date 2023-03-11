1afffb733afaa42b007911573d1f1592e9bc0e79
valerijmedvid <vmedvid@gmail.com>
2023-03-11 15:17:06 +0100

    better behavior of adding new tasks

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTask } from '../slicers/todosListSlice'
 
@@ -9,23 +9,29 @@ type AddTaskProps = {
 }
 export default function AddTask(props: AddTaskProps) {
   const [showInput, setShowInput] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
+
   const dispatch = useDispatch()
 
   function handleKeyPress(event: KeyboardEvent<HTMLInputElement>): void {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && inputRef.current?.value != '') {
       dispatch(
         addTask({
           task: { title: event.currentTarget.value, completed: false },
           index: props.todoListIndex,
         }),
       )
-      setShowInput(false)
+      if (inputRef.current != null) {
+        inputRef.current.value = ''
+      }
     }
   }
+  
   return (
     <div className='mt-3'>
       {showInput ? (
         <input
+          ref={inputRef}
           type='text'
           autoFocus
           className='w-32'
